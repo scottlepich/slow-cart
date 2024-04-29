9e869ce795e6a98d918a88a43ac691084edf779b
Scott Lepich <scott@lepich.com>
2024-04-29 07:50:37 -0700

    message and disable add when inventory limit reacthed

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,4 +1,5 @@
 import { FC, PropsWithChildren, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 import Quantity from "./Quantity";
 
@@ -7,6 +8,7 @@ import "./product.css";
 import useCart from "../hooks/use-cart";
 
 export interface Props {
+  quantity: number;
   desc: string;
   id: number;
   name: string;
@@ -14,25 +16,33 @@ export interface Props {
 }
 
 const Product: FC<PropsWithChildren<Props>> = ({
+  quantity: available,
   desc,
   name,
   id,
   thumbnail_url,
 }) => {
   const [count, setCount] = useState<number>(0); // local state count
-  const [isInit, setIsInit] = useState<boolean>(true);
 
-  const { addToCart } = useCart();
-
-  useEffect(() => {
-    setIsInit(false);
-  }, []);
+  const [hasDisabledAdd, setHasDisabledAdd] = useState<boolean>(false);
 
   useEffect(() => {
-    if (!isInit) {
-      addToCart(id, name, count);
+    if (hasDisabledAdd && count < available) {
+      setHasDisabledAdd(false);
     }
-  }, [count]);
+  }, [count, available, hasDisabledAdd]);
+
+  const { updateCart } = useCart();
+
+  const handleAdd = (count: number) => {
+    if (count <= available) {
+      setCount(count);
+      updateCart(id, name, count);
+    } else {
+      toast.error(`Only ${available} ${name} available.`);
+      setHasDisabledAdd(true);
+    }
+  };
 
   return (
     <li>
@@ -41,9 +51,10 @@ const Product: FC<PropsWithChildren<Props>> = ({
         <h3>{name}</h3>
         <p>{desc}</p>
         <Quantity
-          onAddClick={() => setCount(count + 1)}
+          onAddClick={() => handleAdd(count + 1)}
           onRemoveClick={() => setCount(count - 1)}
           count={count}
+          hasDisabledAdd={hasDisabledAdd}
         />
       </div>
     </li>
--- src/components/Quantity.tsx
+++ src/components/Quantity.tsx
@@ -6,17 +6,21 @@ export interface Props {
   onAddClick: MouseEventHandler;
   onRemoveClick: MouseEventHandler;
   count: number;
+  hasDisabledAdd: boolean;
 }
 
 const Quantity: FC<PropsWithChildren<Props>> = ({
   onAddClick,
   onRemoveClick,
   count,
+  hasDisabledAdd,
 }) => (
   <div className="quantity">
     {count > 0 ? (
       <>
-        <button onClick={onAddClick}>Add</button>
+        <button onClick={onAddClick} disabled={hasDisabledAdd}>
+          Add
+        </button>
         <p>{count}</p>
         <button onClick={onRemoveClick}>Remove</button>
       </>
