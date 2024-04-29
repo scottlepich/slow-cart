8068b62cf80148c4c4dc98a8f3a369b3e5b1c6fa
Scott Lepich <scott@lepich.com>
2024-04-28 20:29:52 -0700

    some styling and data updates

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -7,26 +7,33 @@ import "./product.css";
 export interface Props {
   desc: string;
   name: string;
+  thumbnail_url: string;
 }
 
 const handleAddClick = (count: number, add: (n: number) => void) => {
   add(count + 1);
 };
 
-const Product: FC<PropsWithChildren<Props>> = ({ name, desc }) => {
+const Product: FC<PropsWithChildren<Props>> = ({
+  desc,
+  name,
+  thumbnail_url,
+}) => {
   const [countInCart, setCountInCart] = useState<number>(0);
 
   return (
     <li>
-      <h3>{name}</h3>
-      <p>{desc}</p>
-      <p>Added: {countInCart}</p>
-      <Quantity
-        onClick={() => handleAddClick(countInCart, setCountInCart)}
-        count={countInCart}
-      >
-        Add
-      </Quantity>
+      <img src={thumbnail_url} />
+      <div>
+        <h3>{name}</h3>
+        <p>{desc}</p>
+        <p>Cart: {countInCart}</p>
+
+        <Quantity
+          onClick={() => handleAddClick(countInCart, setCountInCart)}
+          count={countInCart}
+        />
+      </div>
     </li>
   );
 };
